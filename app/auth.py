2f5cb0ba2c3dd60d8b76ab00dae265c62bd2f8d5
RaY8118 <parthghadi43@gmail.com>
2024-08-19 20:04:26 +0530

    Updated `generate_custom_id` function to assign IDs based on the count of existing documents with the same prefix

--- app/auth.py
+++ app/auth.py
@@ -1,14 +1,23 @@
 from flask import jsonify
 from app import mongo, bcrypt
+import re
+
 
 user_collection = mongo.db.users
+
+def generate_custom_id():
+    prefix = "USID"
+    count = user_collection.count_documents({})
+    number = count + 1 
+    return f"{prefix}{str(number).zfill(3)}"
 
 def register_user(request):
     data = request.json
     name = data.get('name')
     email = data.get('email')
+    mobile = data.get('mobile')
     password = data.get('password')
-    role = data.get('role')
+    role = data.get('value')
 
     hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
     existing_user = user_collection.find_one({"email": email})
@@ -16,11 +25,15 @@ def register_user(request):
     if existing_user:
         return jsonify({"status": "error", "message": "User already exists"}), 400
 
+    custom_id = generate_custom_id()
+    
     new_user = {
         "name": name,
         "email": email,
+        "mobile": mobile,
         "password": hashed_password,
-        "role": role
+        "role": role,
+        "userId":custom_id
     }
     user_collection.insert_one(new_user)
 
@@ -41,9 +54,10 @@ def login_user(request):
         return jsonify({"status": "error", "message": "Invalid email or password"}), 401
 
     user_data = {
+        "userId":user["userId"],
         "name": user["name"],
         "email": user["email"],
-        "role": user.get("role")
+        "mobile": user["mobile"],
+        "role": user["role"],
     }
     return jsonify({"status": "success", "message": "Login Successful", "user": user_data})
-
