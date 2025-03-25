fca389a80df663fb01dd010678457552ace49e02
RaY8118 <parthghadi43@gmail.com>
2025-03-25 20:53:42 +0530

    WIP: Current changes before testing old code

--- app/chat.py
+++ app/chat.py
@@ -10,6 +10,8 @@ from app import mongo, socketio
 
 rooms_collection = mongo.db.rooms
 messages_collection = mongo.db.messages
+user_collection = mongo.db.users
+families_collection = mongo.db.families
 
 user_sessions = {}
 
@@ -43,6 +45,9 @@ def join_room_api(request):
     print(data)
     room = data.get("room")
     name = data.get("name")
+    caregiver_id = data.get("CGId")
+    patient_id = data.get("PATId")
+    role = data.get("role")
 
     if not room or not name:
         return (
@@ -57,11 +62,23 @@ def join_room_api(request):
     if not room_data:
         return jsonify({"status": "error", "message": "Room not found"}), 404
 
+    caregiver = user_collection.find_one({"userId": caregiver_id})
+    patient = user_collection.find_one({"userId": patient_id})
+    if caregiver["family_id"] != patient["family_id"]:
+        return (
+            jsonify(
+                {
+                    "status": "error",
+                    "message": "You don not have the permission to join this room",
+                }
+            ),
+            400,
+        )
+
     messages_data = messages_collection.find_one({"roomId": room})
     messsages = messages_data["messages"] if messages_data else []
 
-    session["room"] = room
-    session["name"] = name
+    session["user"] = caregiver_id if role == "caregiver" else patient_id
     print(f"Session data: {session}")
     return jsonify(
         {
@@ -79,6 +96,7 @@ def connect():
     sid = request.sid
     room = session.get("room")
     name = session.get("name")
+    user = session.get("user")
 
     if not room or not name:
         send(
@@ -95,10 +113,10 @@ def connect():
         send({"status": "error", "message": "Room not found"}, to=sid)
         return
 
-    user_sessions[sid] = {"room": room, "name": name}
+    user_sessions[sid] = {"room": room, "name": name, "user": user}
     join_room(room)
     send({"name": name, "message": f"{name} has joined the room"}, to=room)
-    rooms_collection.update_one({"room": room}, {"$inc": {"members": 1}})
+    rooms_collection.update_one({"room": room}, {"$inc": {"members": 1}}, upsert=True)
     print(f"{name} joined room {room}")
 
 
@@ -108,6 +126,7 @@ def handle_message(data):
     sid = request.sid
     room = user_sessions.get(sid, {}).get("room")
     name = user_sessions.get(sid, {}).get("name")
+    user = user_sessions.get(sid, {}).get("user")
     message_content = data.get("message")
 
     if not room or not name or not message_content:
@@ -125,6 +144,7 @@ def handle_message(data):
         "name": name,
         "message": message_content,
         "createdAt": ist_time.strftime("%Y-%m-%d %H:%M:%S"),
+        "user": user,
     }
     send(content, to=room)
     messages_collection.update_one(
@@ -146,8 +166,9 @@ def disconnect():
         updated_room = rooms_collection.find_one({"room": room})
 
         # # Delete the room if empty
-        # if updated_room and updated_room["members"] <= 0:
-        #     rooms_collection.delete_one({"room": room})
+        if updated_room and updated_room["members"] <= 0:
+            print(f"Room deleted: {room}")
+            rooms_collection.delete_one({"room": room})
 
         send({"name": name, "message": f"{name} has left the room"}, to=room)
         print(f"{name} has left the room {room}")
